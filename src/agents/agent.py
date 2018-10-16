faa04af1095e14e9ea325bbfd33e1e54760f4272
Jordan Ott <jordanott365@gmail.com>
2018-10-16 14:55:45 -0700

    Agents setup

---
+++ src/agents/agent.py
@@ -0,0 +1,13 @@
+class Agent(object):
+    """Agent super class"""
+    def __init__(self):
+        pass
+
+    def act(self):
+        print('Actions')
+
+    def update(self):
+        pass
+
+    def copy(self):
+        pass
