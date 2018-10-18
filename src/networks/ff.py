0571ed81145d68575983af0a5500f5dce3f38149
Jordan Ott <jordanott365@gmail.com>
2018-10-17 21:38:12 -0700

    Args

--- src/networks/ff.py
+++ src/networks/ff.py
@@ -3,15 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FF(nn.Module):
-    def __init__(self, input_size, num_actions):
+    def __init__(self, input_size, num_outputs):
         super(FF, self).__init__()
         self.fc1 = nn.Linear(input_size, 512)
-        self.fc2 = nn.Linear(512, num_actions)
+        self.fc2 = nn.Linear(512, num_outputs)
 
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p)
-                
+
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
