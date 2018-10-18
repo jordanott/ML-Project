801fb6e1f2a4e7a733ed0e2b27b64109e6a9c172
Jordan Ott <jordanott365@gmail.com>
2018-10-18 15:17:24 -0700

    Tracking episode info

--- Tests/test_env.py
+++ Tests/test_env.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from src.agents.dqn import DQN
+from src.helper import plotting as P
 from src.environment import env_manager, env_helper
 
 EYE_DIM = 64
@@ -15,27 +16,39 @@ env = env_manager.Environment(data_dir='../data/')
 behaviour = DQN(4)
 target = DQN(4)
 
+num_episodes = 0
 
 while True:
-
     s = env.reset(); done = False
-    episode_count = 1
-
+    episode = {
+        'episode_len': 0,
+        'reward': [],
+        'history': []
+    }
     behaviour.reset()
+
     while not done:
         # take action a
         a,w = behaviour.act(s)
         # environment returns new state, reward, done
         s_prime, r, done = env.step(a,w)
+        print(num_episodes, r,done)
 
-        target.remember(s,a,r,s_prime,done)
-
-        target.replay(behaviour.model)
+        # save episode info
+        episode['history'].append([s,a,r,s_prime,done])
+        episode['reward'].append(r); episode['episode_len'] += 1
 
         s = s_prime
+    print('Done')
+    num_episodes += 1
 
-        if episode_count % 100 == 0:
-            env.visualize_eyetrace()
+    target.remember(episode['history']) # s,a,r,s_prime,done)
 
-            behaviour.copy(target.model)
-        episode_count += 1
+    if num_episodes % 10 == 0:
+        target.replay(behaviour.model)
+
+        env.visualize_eyetrace()
+
+        behaviour.copy(target.model)
+
+        P.vs_time(episode['reward'],xlabel='Time',ylabel='Reward',title='Reward vs Time')
