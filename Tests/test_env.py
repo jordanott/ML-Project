0721ba38d11703e46df04228a88392961c6d5e81
Jordan Ott <jordanott365@gmail.com>
2018-10-19 18:08:38 -0700

    Metric monitor, logging occasionally

--- Tests/test_env.py
+++ Tests/test_env.py
@@ -5,50 +5,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from src.agents.dqn import DQN
-from src.helper import plotting as P
+from src.helper.monitor import MetricMonitor
 from src.environment import env_manager, env_helper
 
-EYE_DIM = 64
+# monitor information
+mm = MetricMonitor()
 
 # define env
 env = env_manager.Environment(data_dir='../data/')
-# Human agent allows user to chose actions
-behaviour = DQN(4)
-target = DQN(4)
 
-num_episodes = 0
+# Initialize agents
+behaviour = DQN(4); target = DQN(4)
+behaviour.copy(target.model)
 
 while True:
     s = env.reset(); done = False
-    episode = {
-        'episode_len': 0,
-        'reward': [],
-        'history': []
-    }
+
+    mm.reset_episode()
     behaviour.reset()
 
     while not done:
         # take action a
         a,w = behaviour.act(s)
         # environment returns new state, reward, done
-        s_prime, r, done = env.step(a,w)
-        print(num_episodes, r,done)
+        s_prime, r, done, correct_word = env.step(a,w)
 
         # save episode info
-        episode['history'].append([s,a,r,s_prime,done])
-        episode['reward'].append(r); episode['episode_len'] += 1
+        mm.store(s,a,r,s_prime,done,correct_word)
 
         s = s_prime
-    print('Done')
-    num_episodes += 1
 
-    target.remember(episode['history']) # s,a,r,s_prime,done)
+        if (mm.num_episodes + 1) % 100 == 0:
+            env.visualize_eyetrace(r,reward_over_time=mm.episode['reward'])
 
-    if num_episodes % 10 == 0:
+    target.remember(mm.get_history())
+    mm.end_episode()
+
+    if mm.num_episodes % 10 == 0:
         target.replay(behaviour.model)
-
-        env.visualize_eyetrace()
 
         behaviour.copy(target.model)
 
-        P.vs_time(episode['reward'],xlabel='Time',ylabel='Reward',title='Reward vs Time')
+        mm.log_status()
