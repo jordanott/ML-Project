acd55805cf41f7365c6606a26abc5a7260933d04
Jordan Ott <jordanott365@gmail.com>
2018-10-19 18:09:26 -0700

    Saving plots

--- src/helper/plotting.py
+++ src/helper/plotting.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 
-def vs_time(data,xlabel='',ylabel='',title=''):
+def vs_time(data,xlabel='',ylabel='',title='',save=True,location=''):
+    plt.clf()
+
     plt.plot(data)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.show()
+
+    if save: plt.savefig(location + ylabel + '.png')
+    else: plt.show()
