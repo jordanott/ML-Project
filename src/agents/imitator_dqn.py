ffe98aeae0f70731db27d097527221c6d6e42ed7
Jordan Ott <jordanott365@gmail.com>
2018-10-23 08:46:24 -0700

    Imitate and replay

--- src/agents/imitator_dqn.py
+++ src/agents/imitator_dqn.py
@@ -20,19 +20,14 @@ class Model(nn.Module):
         self.lstm = LSTM(64*29*29, hidden_size=256)
         self.word = FF(input_size=256,num_outputs=num_words)
         self.actions = FF(input_size=256,num_outputs=num_actions)
-        self.frozen = False
-
-    def freeze(self):
-        self.embed.weight.requires_grad=False
-        self.lstm.weight.requires_grad=False
-        self.frozen = True
+        self.IMITATE = True
 
     def forward(self, x):
         e = self.embed(x).unsqueeze(0)
         lstm_out = self.lstm(e)
 
         actions = self.actions(lstm_out)
-        if not self.frozen: actions = F.log_softmax(actions, dim=1)
+        if self.IMITATE: actions = F.log_softmax(actions, dim=1)
 
         word_prediction = self.word(lstm_out)
         word_prediction = F.log_softmax(word_prediction, dim=1)
@@ -42,7 +37,7 @@ class Model(nn.Module):
 class DQN(object):
     def __init__(self,num_actions,num_words=13551,VISUALIZE=False):
         # replay memory
-        self.memory = []
+        self.memory = []; self.imitate_memory = []
         # discount rate
         self.gamma = 0.8
         # exploration rate
@@ -69,24 +64,39 @@ class DQN(object):
         self.model = self.model.to(self.device)
 
     def reset(self):
-        self.model.lstm.reset_hidden()
+        self.model.lstm.reset_hidden(self.device)
 
     def copy(self,model):
         # avoid leaf copy problem
-        model.lstm.reset_hidden()
-        self.model = deepcopy(model)
+        model.lstm.reset_hidden(self.device)
+        self.model.load_state_dict(model.state_dict())
 
-    def remember(self,episode):
-        deleted = 0
+    def act(self,state):
+        state = state.to(self.device)
+        q_values,w = self.model(state)
+
+        w = torch.argmax(w,dim=1)
+        
+        # sample random action with probability epsilon
+        if uniform(0, 1) < self.epsilon:
+            return randint(0,self.num_actions-1),w
+
+        a = torch.argmax(q_values,dim=1).cpu().data.numpy()[0]
+
+        return a,w
+
+    def remember(self,episode,IMITATE=True):
         if len(self.memory) == self.memory_size:
             del self.memory[0]
-        for idx in range(len(episode)):
-            if episode[idx][-1][-1] is None:
-                del episode[idx]; deleted += 1
 
-        print('Word focuses deleted:', deleted)
-        # store experience
-        self.memory.append(episode)
+        if IMITATE:
+            for idx in range(len(episode)):
+                if episode[idx][-1][-1] is None:
+                    del episode[idx]
+
+            self.imitate_memory = episode
+        else:
+            self.memory.append(episode)
 
     def get_batch(self):
         # if replay memory is big enough take random sample
@@ -95,43 +105,80 @@ class DQN(object):
 
         return self.memory
 
-    def replay(self):
-        total_loss = {'action_loss': [], 'word_loss':[]}
+    def replay(self, actor):
+        print('Checking IMITATE for imitator and actor', self.model.IMITATE, actor.IMITATE)
         batch = self.get_batch()
-        for episode in batch: # should only be one episode per batch
-            for word_focus in episode:
-                self.reset() # reset hidden states
-                action_loss_history = []
 
-                for saccade in word_focus:
-                    action_loss, word_loss = 0,0
-                    # get experience from saccade
-                    s,a,r,s_prime,done,correct_word = saccade
+        for episode in batch:
+            self.reset(); actor.lstm.reset_hidden(self.device)
+            for time_step in episode:
+                # get experience from batch
+                s,a,r,s_prime,done,correct_word = time_step
 
-                    # move data to GPU
-                    s, a = s.to(self.device), torch.Tensor([a]).long().to(self.device)
+                # move data to GPU
+                s, a = s.to(self.device), torch.Tensor([a]).long().to(self.device)
 
-                    # network predicted action and word
-                    action_pred,word_pred = self.model(s)
+                # Q(s, a; theta_target)
+                q_s,w = self.model(s)
+                q_s_a = q_s[0,a]
 
-                    # action loss
-                    action_loss = F.nll_loss(action_pred, a)
-                    # record action loss
-                    action_loss_history.append(action_loss.item())
+                # if the round hasn't ended; estimate value of taking best action in s_prime
+                if not done:
+                    s_prime =  s_prime.to(self.device)
+                    # max(Q(s', a'; theta_actor))
+                    q_s_prime, w_prime = actor(s_prime)
+                    q_s_a_prime = torch.max(q_s_prime)
+                    # add to reward with discount factor gamma
+                    r += self.gamma * q_s_a_prime
 
-                    if correct_word: # if its time to predict a word
-                        word_pred = word_pred.to(self.device)
-                        correct_word = torch.Tensor([correct_word]).long().to(self.device)
+                error = r - q_s_a
+                clipped_error = -1.0 * error.clamp(-1,1)
 
-                        word_loss = F.nll_loss(word_pred, correct_word)
-                        # record losses
-                        total_loss['action_loss'].append(np.mean(action_loss_history))
-                        total_loss['word_loss'].append(word_loss.item())
-                        action_loss_history = []
+                q_s_a.backward(clipped_error, retain_graph=True)
+                self.opt.step()
 
-                    # total loss: combination of action and word losses
-                    loss = action_loss + word_loss
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
-                    loss.backward(retain_graph=True)
-                    self.opt.step()
+    def imitate(self):
+        total_loss = {'action_loss': [], 'word_loss':[]}
+
+        for word_focus in self.imitate_memory:
+            self.reset() # reset hidden states
+            action_loss_history = []
+
+            for saccade in word_focus:
+                action_loss, word_loss = 0,0
+                # get experience from saccade
+                s,a,r,s_prime,done,correct_word = saccade
+
+                # move data to GPU
+                s, a = s.to(self.device), torch.Tensor([a]).long().to(self.device)
+
+                # network predicted action and word
+                action_pred,word_pred = self.model(s)
+
+                # action loss
+                action_loss = F.nll_loss(action_pred, a)
+
+                # record action loss
+                action_loss_history.append(action_loss.item())
+
+                if correct_word: # if its time to predict a word
+                    word_pred = word_pred.to(self.device)
+                    correct_word = torch.Tensor([correct_word]).long().to(self.device)
+
+                    word_loss = F.nll_loss(word_pred, correct_word)
+                    # record losses
+                    total_loss['action_loss'].append(np.mean(action_loss_history))
+                    total_loss['word_loss'].append(word_loss.item())
+                    action_loss_history = []
+
+                # total loss: combination of action and word losses
+                loss = action_loss + word_loss
+
+                self.opt.zero_grad()
+                loss.backward(retain_graph=True)
+                self.opt.step()
+
         return total_loss
