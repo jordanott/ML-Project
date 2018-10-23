539da6a542939e4b26fd135405979922a431d64f
Jordan Ott <jordanott365@gmail.com>
2018-10-23 08:44:53 -0700

    Moving to device

--- src/networks/lstm.py
+++ src/networks/lstm.py
@@ -6,20 +6,20 @@ import torch.nn.functional as F
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers=2):
         super(LSTM, self).__init__()
-        
+
         self.hidden_size = hidden_size; self.num_layers = num_layers
 
         # init lstm
         self.rnn = nn.LSTM(input_size, hidden_size, num_layers, dropout=0.2)
-        # specify device
+        # GPU
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # init hidden state
-        self.reset_hidden()
+        self.reset_hidden(self.device)
 
 
-    def reset_hidden(self):
-        self.hidden = (torch.zeros(self.num_layers, 1, self.hidden_size).to(self.device),
-            torch.zeros(self.num_layers, 1, self.hidden_size).to(self.device))
+    def reset_hidden(self,device):
+        self.hidden = (torch.zeros(self.num_layers, 1, self.hidden_size).to(device),
+            torch.zeros(self.num_layers, 1, self.hidden_size).to(device))
 
     def forward(self, inputs):
         x, self.hidden = self.rnn(inputs, self.hidden)
