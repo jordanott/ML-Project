6a76933e63364c1dfc6d08cd08bd72bd62f007a1
Jordan Ott <jordanott365@gmail.com>
2018-10-24 20:43:53 -0700

    Comments, plotting hist, action q values

--- src/helper/monitor.py
+++ src/helper/monitor.py
@@ -28,7 +28,7 @@ class MetricMonitor(object):
         }
         if self.TEACH: self.episode['history'].append([])
 
-    def set_word_targets(self):
+    def set_word_targets(self): # this will be used when training model from scratch (without imitation learning)
         # start with the last word from episode
         replacement_word = self.episode['history'][-1][-1]
 
@@ -44,7 +44,6 @@ class MetricMonitor(object):
 
     def get_history(self):
         #if not self.TEACH: self.set_word_targets()
-
         return self.episode['history']
 
     def store(self, s, a, r, s_prime, done, correct_word):
@@ -52,7 +51,6 @@ class MetricMonitor(object):
             self.episode['history'][-1].append([s,a,r,s_prime,done,correct_word])
             if correct_word:
                 self.episode['history'].append([])
-
         else:
             self.episode['history'].append([s,a,r,s_prime,done,correct_word])
 
@@ -60,37 +58,50 @@ class MetricMonitor(object):
         self.episode['reward'].append(r)
         self.episode['episode_len'] += 1
 
-    def log_status(self, total_loss):
+    def log_status(self, total_loss, epsilon=None):
+        labels = ['Up','Right','Down','Left','New Line','Classify']
+
         if self.TEACH:
+            # store losses from imitation training
             self.imitator_loss['action_loss'].extend(total_loss['action_loss'])
             self.imitator_loss['word_loss'].extend(total_loss['word_loss'])
 
             data = [self.imitator_loss['action_loss'], self.imitator_loss['word_loss']]
             labels = ['Action Loss', 'Word Loss']
+            # plot losses from imitation training
             P.vs_time(data, labels=labels,xlabel='Time',ylabel='Loss',title='Loss vs Time')
+
+            # plot number of words seen in training
+            P.vs_time(self.metrics['num_words_seen'],xlabel='Time',ylabel='Words Seen',title='Words Seen vs Time')
 
             print('Episodes: {} Action loss: {} Word loss: {}'.format(
                 self.num_episodes, np.mean(total_loss['action_loss']),np.mean(total_loss['word_loss'])))
-
-            P.vs_time(self.metrics['num_words_seen'],xlabel='Time',ylabel='Words Seen',title='Words Seen vs Time')
         else:
-            mean_reward = np.mean(self.metrics['reward'][-100:])
-            print('Episodes: {}, Mean Reward (100): {}'.format(self.num_episodes, mean_reward))
+            # mean reward from acting
+            mean_reward = np.mean(self.episode['reward'])
+            max_reward = np.max(self.episode['reward'])
 
+            # plot reward for acting
             P.vs_time(self.metrics['reward'],xlabel='Time',ylabel='Reward',title='Reward vs Time')
+
+            # plot histogram of action q values for the episode
+            P.hist(total_loss, labels, xlabel='Q Values', title='Action Q Values',save=True, location='images/'+self.num_episodes)
+
+            print('Episodes: {}, Mean Reward: {}, Max: {}, Epsilon: {}'.format(
+                self.num_episodes, mean_reward, max_reward, epsilon))
 
         actions = np.array(self.metrics['action_distribution'])
         action_distribution = [actions[:,i] for i in range(6)]
-        labels = ['Up','Right','Down','Left','New Line','Classify']
+        # plot distribution of actions taken for episode
         P.vs_time(action_distribution, labels=labels,xlabel='Time',ylabel='Action Probabilities',title='Action Probabilities vs Time')
 
     def end_episode(self):
+        # store episode (reward, episode length, and num words seen)
         self.metrics['reward'].extend(self.episode['reward'])
         self.metrics['episode_len'].append(self.episode['episode_len'])
         self.metrics['num_words_seen'].append(len(self.episode['history']))
-
+        # calc action distribution
         x = np.array(self.episode['action_distribution'])
-
-
         self.metrics['action_distribution'].append(x / x.sum().astype(np.float))
+        # increment num episodes
         self.num_episodes += 1
