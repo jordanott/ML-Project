5ec22c89d4851e3f072c83bcf5d0bb51f24eadf6
Jordan Ott <jordanott365@gmail.com>
2018-10-26 13:13:11 -0700

    See word probs

--- src/helper/plotting.py
+++ src/helper/plotting.py
@@ -19,7 +19,26 @@ def vs_time(data,labels=None,xlabel='',ylabel='',title='',save=True,location='')
     if save: plt.savefig(location + ylabel + '.png')
     else: plt.show()
 
+def state_word_prediction(state, word_prob, words, n=5, save=True, location=''):
+    plt.clf(); fig=plt.figure()
 
+    ax=fig.add_subplot(1,2,1)
+    ax.axis('off')
+    ax.imshow(state)
+
+    ax=fig.add_subplot(1,2,2)
+    # indexes of n largest values
+    n_largest_idx = word_prob.argsort()[-n:][::-1]
+    # probability values of n largest
+    n_largest_prob = word_prob[n_largest_idx]
+    # word labels of n largest probable words
+    word_labels = [words[i] for i in n_largest_idx]
+
+    ax.bar(np.arange(n), n_largest_prob, tick_label=word_labels)
+    ax.set_xlabel('Word Probabilities')
+
+    if save: plt.savefig(location + ylabel + '.png')
+    else: plt.show()
 
 def hist(data, labels, xlabel='', title='',save=True, location=''):
     data = np.array(data)
